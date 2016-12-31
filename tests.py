7127a44831fa1f82ed44ed10c9c848227f72236a
cponeill <caseyoneill78@hotmail.com>
2016-12-31 11:54:09 -0500

    adding comment code to tests.py file.

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# Adding in unittests for Keyhash microservice.
+
 import os
 import unittest
 import secureServer
@@ -8,14 +10,17 @@ from unittest import mock
 
 class KeyhashTestCase(unittest.TestCase):
 
+    # Getting app to test url.
     def get(self, url):
         self.app = secureServer.app.test_client()
         return self.app.get(url)
 
+    # Testing whethere 402 endpoint is called by the client.
     def test_success(self):
         response = self.get('/get')
         self.assertEqual(response.status_code, 402)
 
+    # Testing whether 200 endpoint is returned after payment received.
     @mock.patch('two1.bitserv.flask.decorator.Payment.contains_payment',
                 return_value=True)
     def test_buy_success(self, *args):
