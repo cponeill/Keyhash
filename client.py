9a1f6bceffe38fefc41cba295f098e0e162d3f3f
Casey O'Neill <caseyoneill78@hotmail.com>
2017-01-24 00:12:29 -0800

    Update client.py

--- client.py
+++ client.py
@@ -13,7 +13,6 @@ __maintainer__ = "cponeill"
 __email__ = "cponeill@blockshare.io"
 
 #!/usr/bin/env python3
-
 from two1.wallet import Wallet
 from two1.bitrequests import BitTransferRequests
 
@@ -23,7 +22,9 @@ requests = BitTransferRequests(wallet)
 server_url = 'http://[::]:9787/'
 
 def get_data():
-
+    """
+    Getting data from server and outputing JSON.
+    """
     password = input()
     sel_url = server_url+'get?password={0}'
     response = requests.get(url=sel_url.format(password))
