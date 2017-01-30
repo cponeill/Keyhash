f3e6ad7ebd0f74308b742e3c1a40b285b004e2ad
cponeill <caseyoneill78@hotmail.com>
2017-01-30 12:40:45 -0500

    Updating and cleaning code to pep8 standards.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Adding in unittests in Keyhash microservice.
+# TO-DO: Add tests for invalid data.
 import os
 import unittest
 import secureServer
@@ -7,24 +8,27 @@ import json
 
 from unittest import mock
 
+
 class KeyhashTestCase(unittest.TestCase):
 
-    # Getting app to test url.
     def get(self, url):
+        # Ensure the app is retrieving a url.
         self.app = secureServer.app.test_client()
         return self.app.get(url)
 
-    # Testing whethere 402 endpoint is called by the client.
     def test_success(self):
+        # Asserting 'get' is requesting 402 endpoint.
         response = self.get('/get')
         self.assertEqual(response.status_code, 402)
 
-    # Testing whether 200 endpoint is returned after payment received.
+    # Adding patch to 'mock' 402 endpoint purchase using a test case.
     @mock.patch('two1.bitserv.flask.decorator.Payment.contains_payment',
                 return_value=True)
     def test_buy_success(self, *args):
+        # Ensuring 402 request retrieves 200 status.
         response = self.get('/get?hash="keyhash"')
         self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
